0aec3774f91741c7dba3768ba1f8798fad0ccd5b
unknown <heard.jacob.7@gmail.com>
2022-06-25 13:38:06 -0400

    Remove unneccesary scheduling logic, rename Repeats to Tasks, minor changes

--- examples/RepeatSchedulerDemo/RepeatSchedulerDemo.ino
+++ examples/RepeatSchedulerDemo/RepeatSchedulerDemo.ino
@@ -1,52 +1,47 @@
-#include "repeat.h"
+#include "task.h"
 
-void func_100ms() {
-    Serial.println("in func_100ms");
-    delay(100);
+// Define some functions to schedule
+void func_1s() {
+    Serial.println("in func_1s");
+    delay( Milliseconds::fromSeconds(1) );
+}
+void func_2s() {
+    Serial.println("-in func_2s");
+    delay( Milliseconds::fromSeconds(2) );
+}
+void func_3s() {
+    Serial.println("--in func_3s");
+    delay( Milliseconds::fromSeconds(3) );
+}
+void func_4s() {
+    Serial.println("---in func_4s");
+    delay( Milliseconds::fromSeconds(4) );
 }
 
-void func_200ms() {
-    Serial.println("-in func_200ms");
-    delay(200);
-}
 
-void func_300ms() {
-    Serial.println("--in func_300ms");
-    delay(300);
-}
-
-void func_400ms() {
-    Serial.println("---in func_400ms");
-    delay(400);
-}
-
-void func_500ms() {
-    Serial.println("----in func_500ms");
-    delay(500);
-}
-
-RepeatScheduler scheduler;
+TaskScheduler scheduler;
 
 void setup() {
     Serial.begin(115200);
 
-    long msDelay = 500;
-    long maxTimes = -1;
+	long msDelay = 500;
+    long maxTimes = 2;
 
-    Repeat *r = new Repeat(func_200ms, msDelay, maxTimes);
-    scheduler.addRepeat(r);
-    r = new Repeat(func_400ms, msDelay, maxTimes);
-    scheduler.addRepeat(r);
-    r = new Repeat(func_100ms, msDelay, maxTimes);
-    scheduler.addRepeat(r);
-    r = new Repeat(func_500ms, msDelay, maxTimes);
-    scheduler.addRepeat(r);
-    r = new Repeat(func_300ms, msDelay, maxTimes);
-    scheduler.addRepeat(r);
-
-    // Should normalize to 100, 200, 300, 400, 500
+	Task *t;
+    t = new Task(func_1s, msDelay, maxTimes);
+    scheduler.addTask(t);
+    t = new Task(func_2s, msDelay, maxTimes);
+    scheduler.addTask(t);
+    t = new Task(func_3s, msDelay, maxTimes);
+    scheduler.addTask(t);
+    t = new Task(func_4s, msDelay, maxTimes);
+    scheduler.addTask(t);
 }
 
 void loop() {
-    scheduler.update();    
+    scheduler.update();
+	if(scheduler.isDone()) {
+		Serial.println("Finished all tasks!");
+		while(1);
+	}
 }
